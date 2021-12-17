42355edd17864ad323ca08823498b6e5924fcadd
ChristopherEeles <christopher.eeles@outlook.com>
2021-12-16 20:22:38 -0500

    Add: hello_cargo rust crate

---
+++ projects/hello_cargo/target/rls/debug/deps/hello_cargo-4367642d0aaaaddf.d
@@ -0,0 +1,5 @@
+/home/ceeles/development/rust_practice/projects/hello_cargo/target/rls/debug/deps/hello_cargo-4367642d0aaaaddf.rmeta: src/main.rs
+
+/home/ceeles/development/rust_practice/projects/hello_cargo/target/rls/debug/deps/hello_cargo-4367642d0aaaaddf.d: src/main.rs
+
+src/main.rs:
